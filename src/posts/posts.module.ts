b007b188a466939d218333012ef7a53c1297657b
ChoiSangOk <tkddhr12345@naver.com>
2024-08-16 15:53:39 +0900

    Add: 모듈 추가

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,18 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModel } from './entities/posts.entity';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { UsersModule } from 'src/users/users.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostsModel])],
+  imports: [
+    TypeOrmModule.forFeature([PostsModel]),
+    AuthModule, // AuthModule 추가
+    UsersModule, // UsersModule 추가
+  ],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, AccessTokenGuard],
+  exports: [PostsService],
 })
 export class PostsModule {}
